a4cdd1314bcc810986be9d6d8a5abc89960bcdab
sabbir <aiubsakib@gmail.com>
2023-02-09 12:15:33 +0100

    push

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+   version = "~> 2.29.0" 
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-cloud"
+    storage_account_name = "storagecontainer88"
+    container_name       = "terraformtffile"
+    key                  = "terraform.tfstate"
+  }
+}
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
+
+
 
