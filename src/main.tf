5d40380fb25bcbe3badd971a5c980ef3da3753ad
sabbir <aiubsakib@gmail.com>
2023-02-10 08:21:17 +0100

    push

--- src/main.tf
+++ src/main.tf
@@ -22,6 +22,29 @@ resource "azurerm_resource_group" "rg" {
   name     = "myterraform"
   location = "West Europe"
 }
+resource "azurerm_app_service_plan" "appplan" {
+  name = "sakib-appserviceplan"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    size = "S1"
+    tier = "Standard"
+  }
+ 
+}
+
+resource "azurerm_app_service" "appservice" {
+  name = "sakib-appservice"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appplan.id
+   https_only            = true
+  site_config { 
+    dotnet_framework_version = "v4.0"
+    remote_debugging_enabled = true
+    remote_debugging_version = "VS2019"
+}
+}
 
 
 
